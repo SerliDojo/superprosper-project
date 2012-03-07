795a4b7b58ca7779bcb69eba98eb969d783fc99a
Laurent <laurent.ruaud@serli.com>
2012-03-07 10:29:32 +0000

    Finalisation des scripts SQL

---
+++ quickly-data-base/src/main/resources/delete-tables.sql
@@ -0,0 +1,15 @@
+---
+--- SUPPRESSION DE CLEFS ETRANGERES
+---
+alter table CONTRAT drop constraint FK_CONTRAT_NUMERO;
+alter table PROSPECT drop constraint FK_PROSPECT_CLIENT;
+alter table PROSPECT drop constraint FK_PROSPECT_AGENT;
+
+---
+--- SUPPRESSION DES TABLES
+---
+drop table AGENT;
+drop table CLIENT;
+drop table CONTRAT;
+drop table PROSPECT;
+drop table TEXTES;
