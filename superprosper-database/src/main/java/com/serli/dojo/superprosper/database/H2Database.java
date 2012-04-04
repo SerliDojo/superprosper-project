ada801225fc402b8d6ecdb44125ad0317ddb3d19
Laurent <laurent.ruaud@serli.com>
2012-04-04 23:49:41 +0000

    Ajout du lancement du serveur TCP et de la console Web

--- superprosper-database/src/main/java/com/serli/dojo/superprosper/database/H2Database.java
+++ superprosper-database/src/main/java/com/serli/dojo/superprosper/database/H2Database.java
@@ -6,7 +6,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import org.h2.tools.Console;
 import org.h2.tools.RunScript;
+import org.h2.tools.Server;
 
 public class H2Database {
 
@@ -15,12 +17,18 @@ public class H2Database {
 		if (args.length > 0)
 			filename = args[0];
 		Class.forName("org.h2.Driver");
-		Connection sqlConnection = DriverManager.getConnection("jdbc:h2:" + filename);
+		Connection sqlConnection = DriverManager.getConnection("jdbc:h2:" + filename, "dojo", "dojo");
 
 		RunScript.execute(sqlConnection, new StringReader("DROP ALL OBJECTS;"));
 		for (String file : new String[] { "schema/schema.sql", "dataset/data-textes.sql", "dataset/data-agents.sql",
-				"dataset/data-clients.sql", "dataset/data-prospections.sql", "dataset/data-contrats.sql", "dataset/data-contrats-produits.sql" }) {
-			RunScript.execute(sqlConnection, new InputStreamReader(H2Database.class.getClassLoader().getResourceAsStream(file)));
+				"dataset/data-clients.sql", "dataset/data-prospections.sql", "dataset/data-contrats.sql",
+				"dataset/data-contrats-produits.sql" }) {
+			RunScript.execute(sqlConnection, new InputStreamReader(H2Database.class.getClassLoader()
+					.getResourceAsStream(file)));
 		}
+
+		Server.createTcpServer(new String[0]).start();
+		Server.createWebServer(new String[0]).start();
+		Console.main("-url", "jdbc:h2:tcp://localhost/" + filename, "-user", "dojo", "-password", "dojo");
 	}
 }
